a0efc23dba8e43e155b7dad1abce33d12dc1f7a7
NikiRaykov <79482102+NikiRaykov@users.noreply.github.com>
2021-07-11 09:29:44 +0200

    Add files via upload

---
+++ Bank App/Bank.java
@@ -0,0 +1,10 @@
+public class Bank {
+    public static void main(String[] args) {
+
+        Account niki = new Account("Niki Raykov", "BG199600");
+        niki.showMenu();
+
+        Account yana = new Account("Yana Borisova", "Y200015");
+        //yana.showMenu();
+    }
+}
